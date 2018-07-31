592f63a5f9b22afd3d6e111a5fb8324754b7d0c7
Isaiah Harris <harrisfambam@gmail.com>
2018-07-30 15:28:50 -1000

    drop completed

--- todo_app.sql
+++ todo_app.sql
@@ -1,6 +1,23 @@
 DROP DATABASE IF EXISTS todo_app;
+
+DROP USER IF EXISTS michael;
+
+
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+
 CREATE DATABASE todo_app;
 
-CREATE ROLE michael;
-DROP ROLE IF EXISTS michael;
-CREATE ROLE michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+\c todo_app;
+
+
+CREATE TABLE tasks(
+id integer NOT NULL,
+title varchar(255) NOT NULL,
+description text,
+created_at timestamp without time zone NOT NULL DEFAULT now(),
+updated_at timestamp without time zone,
+completed boolean NOT NULL DEFAULT false,
+PRIMARY KEY (id)
+);
+
+ALTER TABLE tasks DROP completed;
